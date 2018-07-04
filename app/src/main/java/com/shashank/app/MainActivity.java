579b1cd6a48a0aa65482987a0acaf1b803accd61
Shashank Pujari <shashank-p@users.noreply.github.com>
2018-07-04 12:10:02 +0530

    Initial commit

---
+++ app/src/main/java/com/shashank/app/MainActivity.java
@@ -0,0 +1,45 @@
+package com.shashank.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // REMOTE RESOURCE
+        mWebView.loadUrl("http://www.symist.com/");
+        mWebView.setWebViewClient(new MyWebViewClient());
+
+        // LOCAL RESOURCE
+        //mWebView.loadUrl("file:///android_asset/index.html");
+    }
+
+    // Prevent the back-button from closing the app
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
